62b41b795f5f393a172aa03dbee4927d6964ea86
malobr <marcelotc1202@gmail.com>
2024-05-29 09:10:17 -0300

    ATualizacvao codigo

--- APIC--Quinta/API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ APIC--Quinta/API/API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+53def03cd65ddda8be6d1f2852ea24bb35fc8c37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+54a89a533b8827967c25135d4e3636485ad0b5e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
